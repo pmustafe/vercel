09b43ef847a6a4aa0bfd87f921379082a825ccb6
pmustafe <pedromus23@gmail.com>
2025-08-20 22:31:43 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#022674",
+        light: "#ffffff"
+      }
+    },
+  },
+  plugins: [],
+}
